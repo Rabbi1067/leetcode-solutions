8ad48df1a70a6a1fcb686044e730e452c9c001ea
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-08 09:20:12 +0600

    Create 559. Maximum Depth of N-ary Tree.cpp

---
+++ 559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr)
+            return 0;
+
+        int maxChildDepth = 0;
+        for (Node* child : root->children) {
+            maxChildDepth = max(maxChildDepth, maxDepth(child));
+        }
+
+        return maxChildDepth + 1;
+    }
+};
